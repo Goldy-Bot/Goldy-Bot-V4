6c50081cf3fd96531ca5b3a8d2c670542bbf538e
Dev Goldy <jassim7256@gmail.com>
2022-03-19 20:24:17 +0000

    hiding run.py

--- src/run.py
+++
@@ -1,8 +0,0 @@
-import GoldyBot
-
-Goldy = GoldyBot.Goldy
-
-#config_file = GoldyBot.config.Config(GoldyBot.files.File(GoldyBot.paths.GOLDY_CONFIG_JSON))
-#print(config_file.read("help_cmd"))
-
-Goldy.start()
