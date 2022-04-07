ca8a8766b6a3c55d1e485fe4dd30a19da671f0e9
Dev Goldy <jassim7256@gmail.com>
2022-04-07 23:40:37 +0100

    more progress on guild class

--- src/GoldyBot/utility/guilds/config.py
+++ src/GoldyBot/utility/guilds/config.py
@@ -1,12 +1,13 @@
+import GoldyBot
 
 class GuildConfig():
     """This class gives you access to all the configuration settings a Goldy Bot guild possess."""
-    def __init__(self):
-        pass
+    def __init__(self, guild_config:GoldyBot.config.Config):
+        self.guild_config = guild_config
 
     @property
     def prefix(self) -> str:
-        pass
+        return self.guild_config.read("prefix")
 
     #TODO: Finish this!
 
--- src/GoldyBot/utility/guilds/guild.py
+++ src/GoldyBot/utility/guilds/guild.py
@@ -1,17 +1,32 @@
 import nextcord
 import GoldyBot
 
+GUILD_CONFIG_TEMPLATE_JSON_PATH = GoldyBot.paths.GOLDY_BOT + "/utility/guilds/guild_config_template.json"
+
 class Guild():
     """Class that represents a discord guild in Goldy Bot."""
     def __init__(self, guild:nextcord.Guild):
         self.guild = guild
+        self.config_file = None
+
+        # Get code name.
+        goldy_config = GoldyBot.config.Config(GoldyBot.files.File(GoldyBot.paths.GOLDY_CONFIG_JSON))
+        self.code_name_ = goldy_config.read("guilds")[f"{self.id}"]
 
     def setup(self):
         """Setup's the given guild in Goldy Bot with a nextcord guild object."""
         # Generate config folder for guild of there isn't already.
         #-----------------------------------
 
-        # Do ya thing.
+        # Create guild config folder.
+        GUILD_CONFIG_FOLDER_PATH = GoldyBot.files.File(GoldyBot.paths.CONFIG + f"/{self.code_name}").file_path
+
+        # Create guild config file.
+        self.config_file = GoldyBot.files.File(GUILD_CONFIG_FOLDER_PATH + "/config.json")
+
+        # If the config is empty, write the guild config template to it.
+        if self.config_file.read() == "":
+            self.config_file.write(GoldyBot.files.File(GUILD_CONFIG_TEMPLATE_JSON_PATH).read())
 
 
         # Create a database collection for the guild if there isn't already.
@@ -25,8 +40,13 @@ class Guild():
         return self.guild.id
 
     @property
+    def code_name(self):
+        return self.code_name_
+
+    @property
     def config(self) -> GoldyBot.utility.guilds.config.GuildConfig:
         """Returns guild's config class."""
+        return GoldyBot.utility.guilds.config.GuildConfig(GoldyBot.config.Config())
         pass
 
     @property
