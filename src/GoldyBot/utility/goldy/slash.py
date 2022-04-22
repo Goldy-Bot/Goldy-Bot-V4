3c110912d2840e1cf9c8be0c3b21ce9adfb50c44
Dev Goldy <jassim7256@gmail.com>
2022-04-22 23:09:48 +0100

    fixed #27

--- src/GoldyBot/utility/goldy/slash.py
+++ src/GoldyBot/utility/goldy/slash.py
@@ -1,9 +1,13 @@
+import asyncio
 import nextcord
 
 Interaction = nextcord.Interaction
 MISSING = nextcord.utils.MISSING
 
+loop = asyncio.get_event_loop()
+
 class InteractionToCtx():
+    """This Goldy Bot class is used to convert interactions to ctx."""
     def __init__(self, interaction: Interaction):
         self.interaction = interaction
 
@@ -13,8 +17,8 @@ class InteractionToCtx():
 
     @property
     def author(self):
-        #TODO: Finish working on this.
-        return self.interaction.message.author
+        #TODO: #27 'interaction.message' is always none.
+        return self.interaction.user
 
     async def send(self, text=None, embed=MISSING):
         await self.interaction.response.send_message(text, embed=embed)
