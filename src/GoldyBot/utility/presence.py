372f20cfbbe128990dc93c5d644a4b5f92985c38
THEGOLDENPRO <jassim7256@gmail.com>
2022-04-27 11:48:31 +0100

    save

---
+++ src/GoldyBot/utility/presence.py
@@ -0,0 +1,8 @@
+import GoldyBot
+
+async def change():
+    """Commands Goldy Bot to change presence."""
+    client:GoldyBot.nextcord.Client = GoldyBot.cache.main_cache_dict["client"]
+
+    
+    pass
